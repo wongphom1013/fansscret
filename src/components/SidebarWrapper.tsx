17214f13acf0d3d1b8dd5b3aeb6ca99b2a83f5e1
wongphom1013 <surachat1013@gmail.com>
2024-12-01 05:34:55 +0700

    update sidebarwrapper.tsx

--- src/components/SidebarWrapper.tsx
+++ src/components/SidebarWrapper.tsx
@@ -1,21 +1,21 @@
-'use server';
+// 'use server';
 
-import Sidebar from './Sidebar';
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { getUserProfileAction } from "@/app/update-profile/actions";
+// import Sidebar from './Sidebar';
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+// import { getUserProfileAction } from "@/app/update-profile/actions";
 
-const SidebarWrapper = async ({ id }: { id: string }) => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-  const userProfile = await getUserProfileAction();
+// const SidebarWrapper = async ({ id }: { id: string }) => {
+//   const { getUser } = getKindeServerSession();
+//   const user = await getUser();
+//   const userProfile = await getUserProfileAction();
   
-  return (
-    <Sidebar
-      id={id}
-      user = {user}
-      userProfile={userProfile}
-    />
-  );
-};
+//   return (
+//     <Sidebar
+//       id={id}
+//       user = {user}
+//       userProfile={userProfile}
+//     />
+//   );
+// };
 
-export default SidebarWrapper;
+// export default SidebarWrapper;
